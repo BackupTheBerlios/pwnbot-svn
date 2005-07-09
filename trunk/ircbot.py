0cf75800edaf60f2dcf0f5b02f9db1750823898a
tiax <tiax@d6d194cf-1dfa-0310-bd9b-a8797cf2af1c>
2005-07-09 13:54:57 +0000

    docstrings zu allen Funktionen hinzugefügt und einen socket timeout eingeführt

--- trunk/ircbot.py
+++ trunk/ircbot.py
@@ -21,41 +21,43 @@ from socket import socket # für die IRC-Verbindung
 
 class ircverbindung:
     def __init__(self,server,nickname,ident,realname):
+        '''gleich verbinden, wenn die klasse erstellt wird'''
         self.lesebuffer = '' # wir brauchen einen leeren Buffer, in den geschrieben wird. Ein Buffer wird gebraucht, weil nicht alles sofort ankommt bei lag usw
         self.verbinde(server,nickname,ident,realname) # gleich am Anfang wird verbunden
-        
+    
     def verbinde(self,server,nickname,ident,realname):
-        '''erwartet: 
+        '''stellt die Verbindung zum Server her
+        erwartet: 
         server als tuple serverurl,port
         nickname als string
         ident als string
         realname als string
 
         gibt nix zurück aber stellt erst die Verbindung zum IRC-Server her und ruft dann den Verarbeiter auf'''
-        
+    
         self.so = socket() 
         self.so.connect(server)
+        self.so.settimeout(5)
         self.so.send('USER %s * * :%s\r\n' % (ident, realname))
         print 'DEBUG:  >> USER %s * * :%s' % (ident, realname)
         self.so.send('NICK %s\r\n' % nickname)
         print 'DEBUG:  >> NICK %s' % nickname
         self.join('#tiax') # DEBBUG-REMOVEME
-        self.verarbeite_reingehendes()
+        self._verarbeite_reingehendes()
 
-    def verarbeite_reingehendes(self):
-        '''erwartet: garnix
+    def _verarbeite_reingehendes(self):
+        '''liest den Buffer aus und verteilt das Reingehende auf die Funktionen
+        erwartet: garnix
         gibt auch nix zurück, aber liest immer 1024 bytes ein und tut sie zum buffer dazu.
         die letzte zeile wird wieder zurück in den buffer getan und erst nächste mal verarbeitet,
         weil vielleicht nicht die ganze Zeile angekommen ist. So kann die Zeile erstmal vervollständigt werden.
-
         Wenn eine entsprechende Funktion besteht (on_FUNKTION), wird diese aufgerufen, sonst wird on_UNBEKANNT aufgerufen.
         als Spezialfall wird hier gleich PING behandelt, denn da sollte tunlichst sofort PONG zurück geschickt werden.'''
-        
+    
         while 1:
             self.lesebuffer = self.so.recv(1024)
             temp = self.lesebuffer.split('\n')
             self.lesebuffer = temp.pop()
-
             for zeile in temp:
                 zeile = zeile.rstrip().split()
                 print 'DEBUG: <<%s' % zeile
@@ -69,27 +71,37 @@ class ircverbindung:
                         befehl(zeile)
                     except AttributeError:
                         self.on_UNBEKANNT(zeile)
-
     def rawsend(self,rausgehendes):
-        '''erwartet:
+        '''schickt Daten an den Server
+        erwartet:
         das, was geschickt werden soll
         gibt auch nix zurück, erspart uns aber die lästige Fehlersuche, wenn die Zeichen am Zeilenende vergessen worden sind.'''
         self.so.send('%s\r\n' % rausgehendes)
-
+    
     def join(self,channel,key=''):
+        '''betritt Channel'''
         print 'DEBUG: >> Joine %s' % channel
         self.rawsend('JOIN %s %s' % (channel, key))
-        
+    
     def msg(self,ziel,nachricht):
+        '''schickt Nachrichten raus'''
         print 'DEBUG: >> PRIVMSG an %s: %s' % (ziel, nachricht)
         self.rawsend('PRIVMSG %S :%s' % (ziel, nachricht))
-        
+    
+    def quit(self,quitmessage):
+        self.rawsend('quit :%s' % quitmessage)
+        self.so.close()
+    
     def on_001(self,*args):
-        self.join('#tiax')
-
+        '''die IRC Verbindung ist gerade hergestellt worden'''
+        self.join('#support')
+    
     def on_PRIVMSG(self,*args):
-        if args()[3].lstrip(':') == 'die':
-            self.rawsend('quit')
+        '''bearbeitet eingehende Nachrichten'''
+        print args
+        #if args[3].lstrip(':') == 'die':
+        #    self.rawsend('quit')
     
     def on_UNBEKANNT(self,*args):
+        '''verarbeitet alles, was nicht sonstwie verarbeitet werden kann'''
         pass
