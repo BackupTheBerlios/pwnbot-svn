a0eb17904a2036f7a811e4dd2cc8ca69be739a96
tiax <tiax@d6d194cf-1dfa-0310-bd9b-a8797cf2af1c>
2005-08-02 03:48:44 +0000

    docstrings aktualisiert

--- trunk/log.py
+++ trunk/log.py
@@ -6,14 +6,22 @@ from sys import exit
 from time import strftime
 class EinzelDateiLogger:
     def __init__(self,dateiname):
-        '''öffnet die Datei'''
+        '''öffnet die Datei
+
+        erwartet: dateiname des Logfiles. Das Verzeichnis in dem es liegt, muss existieren!'''
         try:
             self.logdatei = open(dateiname,'a',1)
         except IOError:
             exit('Verzeichnis nicht vorhanden')
 
     def log(self,kategorie,nachricht):
-        '''erstellt erst ein Datum und schreibt dann in die Datei'''
+        '''erstellt erst ein Datum und schreibt dann in die Datei
+        
+        erwartet: 
+        kategorie als string
+        nachricht als string
+        
+        gibt zurück: nichts, schreibt aber im Format YYYY-MM-DD HH:MM:SS | Kategorie | Hier die Nachricht in die Datei. Dabei werden in der Nachricht alle | durch / ersetzt.'''
         datum = strftime('%Y-%m-%d %H:%M:%S')
         self.logdatei.write('%s|%s|%s\n' % (datum,kategorie,nachricht.replace('|','/')))
     
