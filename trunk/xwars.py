e394dc13c2052755a9729434bb36593334bf23d3
tiax <tiax@d6d194cf-1dfa-0310-bd9b-a8797cf2af1c>
2006-02-09 11:39:36 +0000

    html klappt

--- trunk/xwars.py
+++ trunk/xwars.py
@@ -51,8 +51,8 @@ class kampfbericht:
             self.bericht =  bericht
 
     def _get_daten(self):
-        startregexp = compile(r'Startbasis <b> (.+)</b></br>&nbsp;von ?(\[.+\])? ?(.+)&nbsp;')
-        zielregexp = compile(r'Zielbasis <b> (.+)</b> </br>von&nbsp; ?(\[.+\])? ?(.+)&nbsp;')
+        startregexp = compile(r'Startbasis <b>.*(\d{1,2}x\d{1,3}x\d{1,3})</b></br>&nbsp;von ?(\[.+\])? ?(.+)&nbsp;')
+        zielregexp = compile(r'Zielbasis <b>.*(\d{1,2}x\d{1,3}x\d{1,3})</b> </br>von&nbsp; ?(\[.+\])? ?(.+)&nbsp;')
         uhrzeitregexp = compile(r'Zeit <b>(\d+.\d+.\d+) (\d+:\d+:\d+.*)</b>')
         self.startbasis['Koordinaten'],self.startbasis['Allianz'], self.startbasis['Name'] = startregexp.search(self.bericht).groups()
         self.zielbasis['Koordinaten'], self.zielbasis['Allianz'], self.zielbasis['Name'] = zielregexp.search(self.bericht).groups()
@@ -83,9 +83,102 @@ class kampfbericht:
         ZielbasisVerluste = (abs(self.zielbasis['Flotte'][0] - self.zielbasis['Flotte'][2]), abs(self.zielbasis['Flotte'][1] - self.zielbasis['Flotte'][3]))
         self.startbasis['Verluste'] = StartbasisVerluste
         self.zielbasis['Verluste'] = ZielbasisVerluste
-        self.startbasis['MPVerluste'] = (StartbasisVerluste[0] + StartbasisVerluste[1]) / 200
-        self.zielbasis['MPVerluste'] = (ZielbasisVerluste[0] + ZielbasisVerluste[1]) / 200
+        self.startbasis['MPVerluste'] = (StartbasisVerluste[0] + StartbasisVerluste[1]) / 200.0
+        self.zielbasis['MPVerluste'] = (ZielbasisVerluste[0] + ZielbasisVerluste[1]) / 200.0
 
+    def _make_template(self):
+        template = '''</table><table border="0" cellspacing="0" cellpadding="0" width="665px"><tr><td height="12px"></td></tr><tr>
+		        <td width="282px"></td>
+		        <td width="45px" height="9" bgcolor="#2A2A2A"></td>
+		        <td width="12px"></td>
+		        <td width="45px" xbgcolor="#2A2A2A"></td>
+		        <td width="281px"></td>
+		  </tr>
+		  <tr>
+		        <td xwidth="282" colspan="2" bgcolor="#2A2A2A" height="35px">&nbsp;<b>Statistiken</b>
+
+				<br>&nbsp;<font color="#CCCCCC">Verluste</td>
+		        <td width="12px"></td>
+		        <td xwidth="326px" colspan="2" xbgcolor="#2A2A2A"><font color="#222222">TDM Warreport v1 py</td>
+		  </tr>
+               </table>
+		    
+		<table border="0" cellspacing="0" cellpadding="0" width="665px">
+		<tr><td bgcolor="#2A2A2A" height="9px"></td></tr>
+		<tr>
+
+			<td width= "5px" bgcolor="#2A2A2A">&nbsp;</td>
+            <td width=  "5px" ></td>
+            <td><b>&nbsp;Werte vorher</td>
+			<td><b>&nbsp;nachher</b></td>
+			<td><b>&nbsp;Milit&auml;rpunkte vorher</b></td>
+            <td><b>nachher</b></td>
+			<td><b>Verlust</b></td>
+         </tr>
+         <tr><td bgcolor="#2A2A2A" height="3"></td></tr>
+        <tr><td bgcolor="#444444" height="3"></td></tr>
+        <tr><td bgcolor="#444444" height="1px"></td><td colspan="12" bgcolor="#000000"></tr>
+        <tr height="18px">
+			<td width="50px" bgcolor="#444444">&nbsp;Angreifer</td>
+            <td width=  "5px" ></td>
+		    <td bgcolor="#1A1A1A">&nbsp;%(Angreiferwertevorher)s</td>
+		    <td bgcolor="#1A1A1A">%(Angreiferwertenachher)s</td>
+		    <td bgcolor="#1A1A1A">%(Angreifermpvorher).2f</td>
+		    <td bgcolor="#1A1A1A">%(Angreifermpnachher).2f</td>
+		    <td bgcolor="#1A1A1A">%(Angreifermpverlust).2f</td>
+		</tr>
+		<tr><td bgcolor="#444444" height="1px"></td><td colspan="12" bgcolor="#000000"></tr>
+		<tr><td bgcolor="#2A2A2A" height="9"></td></tr>
+        <tr height="18px">
+			<td width="50px" bgcolor="#444444">&nbsp;Verteidiger</td>
+            <td width=  "5px" ></td>
+		    <td bgcolor="#1A1A1A">&nbsp;%(Verteidigerwertevorher)s</td>
+		    <td bgcolor="#1A1A1A">%(Verteidigerwertenachher)s</td>
+		    <td bgcolor="#1A1A1A">%(Verteidigermpvorher).2f</td>
+		    <td bgcolor="#1A1A1A">%(Verteidigermpnachher).2f</td>
+		    <td bgcolor="#1A1A1A">%(Verteidigermpverlust).2f</td>
+		</tr>
+		<tr><td bgcolor="#444444" height="1px"></td><td colspan="12" bgcolor="#000000"></tr>
+        <tr><td bgcolor="#2A2A2A" height="9"></td></tr>
+</table>
+<table border="0" cellspacing="0" cellpadding="0">
+		<tr>
+		    <td width="50px"  bgcolor="#2A2A2A" height="35px"><br>&nbsp;</td>
+		    <td width="65px"  bgcolor="#2A2A2A" align="right"><font color="CCCCCC">&nbsp;<br>&nbsp;</td>
+		    <td width="20px"  bgcolor="#2A2A2A"><font color="#888888"><br></td>
+		    <td width="146px" bgcolor="#2A2A2A"><font color="#CC5500">&nbsp;&nbsp;<br>&nbsp;&nbsp;<br></td>
+		    <td width="45px"  bgcolor="#2A2A2A" align="right"><b>&nbsp;&nbsp;<br>&nbsp;&nbsp;</td>
+		</tr>
+</table>
+<table border="0" cellspacing="0" cellpadding="0" width="665px"><tr><td height="12px"></td></tr><tr>''' % {'Angreifername':self.startbasis['Name'],
+        'Angreiferwertevorher':str(self.startbasis['Flotte'][0]) + '/' + str(self.startbasis['Flotte'][1]),
+        'Angreiferwertenachher':str(self.startbasis['Flotte'][2]) + '/' + str(self.startbasis['Flotte'][3]),
+        'Angreifermpvorher':self.startbasis['MP'],
+        'Angreifermpnachher':(self.startbasis['MP'] - self.startbasis['MPVerluste']),
+        'Angreifermpverlust':self.startbasis['MPVerluste'],
+        'Verteidigername':self.zielbasis['Name'],
+        'Verteidigerwertevorher':str(self.zielbasis['Flotte'][0]) + '/' + str(self.zielbasis['Flotte'][1]),
+        'Verteidigerwertenachher':str(self.zielbasis['Flotte'][2]) + '/' + str(self.zielbasis['Flotte'][3]),
+        'Verteidigermpvorher': self.zielbasis['MP'],
+        'Verteidigermpnachher': (self.zielbasis['MP'] - self.zielbasis['MPVerluste']),
+        'Verteidigermpverlust': self.zielbasis['MPVerluste']
+        }
+        self.template = template
+
+    def _insert_template(self):
+        berichtliste = self.bericht.splitlines()
+        del berichtliste[berichtliste.index('                <td bgcolor="#2A2A2A" align="right">Gesamt&nbsp;<br>(netto)&nbsp;</td>') + 2]
+        berichtliste.insert(berichtliste.index('                <td bgcolor="#2A2A2A" align="right">Gesamt&nbsp;<br>(netto)&nbsp;</td>') + 1,self.template)
+        self.bearbeiteterbericht = berichtliste
+
+    def manipulate(self):
+        koordinatenregexp = compile(r'\d{1,2}x\d{1,3}x\d{1,3}')
+        bericht = '\n'.join(self.bearbeiteterbericht)
+        bericht = koordinatenregexp.sub('',bericht)
+        bericht = bericht.replace('Login','The Dominion')
+        bericht = bericht.replace('auf Planet','auf einem Planeten')
+        self.bearbeiteterbericht = bericht.splitlines()
+        
     def save(self):
         berichtdatei = open(savedir + self.id,'w')
-        berichtdatei.write(self.bericht)
+        berichtdatei.writelines(self.bearbeiteterbericht)
